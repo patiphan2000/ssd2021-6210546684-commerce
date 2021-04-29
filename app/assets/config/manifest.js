b58c5732d89ee5a92a7449af0e710a83d2f940d1
patiphan2000 <patiphan.sri@ku.th>
2021-04-29 16:06:45 +0700

    Improve show UI

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= link product_index.css
+//= link product_show.css
+//= link product_show.js
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module SsdCommerce
     # Initialize configuration defaults for originally generated Rails version.
     config.assets.initialize_on_precompile = false
     config.load_defaults 6.1
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
